0b10bc3ec85461b2e5b4ba47f1f95e11c639ddd1
Leonardo <lnrdoliveira.dev@gmail.com>
2023-07-20 14:08:28 -0300

    Pequena interação para levar o usuário a operação que ele tem interesse

--- Program.cs
+++ Program.cs
@@ -13,21 +13,42 @@ namespace Controle{
             p.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
             System.Console.Write("Quantidade no estoque: ");
             p.Quantidade = int.Parse(Console.ReadLine());
-
             System.Console.WriteLine("Dados do produto: "+p);
-            System.Console.WriteLine("");
-            System.Console.Write("Digite a quantidade de produtos a serem adicionados: ");
-            int qte = int.Parse(Console.ReadLine());
-            p.AdicionarProdutos(qte);
-            System.Console.WriteLine("");
-            System.Console.WriteLine("Dados atualizados: "+p);
-            System.Console.WriteLine("");
-            System.Console.Write("Digite a quantidade de produtos a serem removidos: ");
-            qte = int.Parse(Console.ReadLine());
-            p.RemoverProdutos(qte);
-            System.Console.WriteLine("Dados atualizados: "+p);
 
+            System.Console.WriteLine("Fazer atualização no estoque (1):");
+            System.Console.WriteLine("Sair (2)");
+            System.Console.Write("Digite aqui: ");
+            int Resposta, qte;
+            Resposta = int.Parse(Console.ReadLine());
 
+            if (Resposta == 1){
+                System.Console.WriteLine("Adicionar (1):");
+                System.Console.WriteLine("Remover (2):");
+                System.Console.WriteLine("Sair (3): ");
+                System.Console.Write("Digite aqui: ");
+                int RespostaEstoque;
+                RespostaEstoque = int.Parse(Console.ReadLine());
+                if(RespostaEstoque == 1){
+                    System.Console.Write("Digite a quantidade de produtos a serem adicionados: ");
+                    qte = int.Parse(Console.ReadLine());
+                    p.AdicionarProdutos(qte);
+                    System.Console.WriteLine("");
+                    System.Console.WriteLine("Dados atualizados: "+p);
+                }
+                else if (RespostaEstoque == 2){
+                    System.Console.WriteLine("");
+                    System.Console.Write("Digite a quantidade de produtos a serem removidos: ");
+                    qte = int.Parse(Console.ReadLine());
+                    p.RemoverProdutos(qte);
+                    System.Console.WriteLine("Dados atualizados: "+p);
+                }
+                else if (RespostaEstoque == 3 || RespostaEstoque > 3){
+                    
+                }
+            }
+            else if (Resposta == 2){
+                
+            }
         }
     }
 }
